d465cf59fcbd45e4e5e7d0f3e7b95dbe50c1d48d
Ian Eure <ieure@php.net>
2005-05-31 20:03:08 +0000

    Use new header comment block
    git-svn-id: http://svn.php.net/repository/pear/packages/File_Fstab/trunk@187414 c90b9560-bf6c-de11-be94-00142212c4b1

--- Entry.php
+++ Entry.php
@@ -1,24 +1,31 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4: */
-// +----------------------------------------------------------------------+
-// | PHP Version 4                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2003 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 3.0 of the PHP license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available through the world-wide-web at the following url:           |
-// | http://www.php.net/license/3_0.txt.                                  |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Author: Ian Eure <ieure@php.net>                                     |
-// +----------------------------------------------------------------------+
-//
-// $Id$
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * This file contains the class for an entry in a fstab file
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category  File Formats
+ * @package   File_Fstab
+ * @author    Ian Eure <ieure@php.net>
+ * @copyright (c) 2004, 2005 Ian Eure
+ * @license   http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version   Release: @package_version@
+ * @version   CVS:     $Revision$
+ * @link      http://pear.php.net/package/File_Fstab
+ */
 
 
+/**
+ * These defines enumerate the possible types of device entries in the fstab
+ */
 define('FILE_FSTAB_ENTRY_DEVTYPE_BLOCKDEV', 1);
 define('FILE_FSTAB_ENTRY_DEVTYPE_UUID', 2);
 define('FILE_FSTAB_ENTRY_DEVTYPE_LABEL', 3);
@@ -27,10 +34,14 @@ define('FILE_FSTAB_ENTRY_DEVTYPE_LABEL', 3);
 /**
  * A single entry in a fstab file
  *
- * @package @package@
- * @version @version@
- * @author Ian Eure <ieure@php.net>
- * @license PHP License v3.0
+ * @category  File Formats
+ * @package   File_Fstab
+ * @author    Ian Eure <ieure@php.net>
+ * @copyright (c) 2004, 2005 Ian Eure
+ * @license   http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version   Release: @package_version@
+ * @version   CVS:     $Revision$
+ * @link      http://pear.php.net/package/File_Fstab
  */
 class File_Fstab_Entry {
     /**
@@ -298,13 +309,13 @@ class File_Fstab_Entry {
         $this->fsckPassNo = $pass;
         return true;
     }
-    
+
     /**
      * Get filesystem fsck pass number
      *
      * @since   2.0.0beta1
      * @return  int
-     */   
+     */
     function getFsckPassNo()
     {
         return $this->fsckPassNo;
@@ -351,11 +362,11 @@ class File_Fstab_Entry {
             case 'UUID':
                 list($null, $this->uuid) = explode('=', $device);
                 break;
-            
+
             case 'LABE':
                 list($null, $this->label) = explode('=', $device);
                 break;
-                
+
             default:
                 $this->device = $device;
         }
@@ -365,7 +376,7 @@ class File_Fstab_Entry {
         $this->_haveParsed = true;
         return true;
     }
-    
+
     /**
      * Clean up prior to parsing
      *
@@ -455,7 +466,7 @@ class File_Fstab_Entry {
             return $this->getLabel();
         }
     }
-    
+
     /**
      * Get device entry for building fstab
      *
@@ -508,7 +519,7 @@ class File_Fstab_Entry {
     {
         return @array_key_exists($option, $this->mountOptions);
     }
-    
+
     /**
      * Get a mount option
      *
@@ -522,7 +533,7 @@ class File_Fstab_Entry {
         }
         return $this->mountOptions[$which];
     }
-    
+
     /**
      * Set a mount option
      *
